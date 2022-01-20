80d6fd03c8ccc851e57299fad63be64501381377
lucas.sousa <lucas.sousa@codificar.com.br>
2022-01-19 22:28:37 -0300

    vai

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="citacao">
+
+    <slot name="fonte"></slot>
+    <slot></slot>
+  <div class="autor">
+      <!-- <slot name="autor"></slot> -->
+  </div>
+  <slot name="autor"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["texto"],
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+h1 {
+  color: red;
+}
+
+.autor{
+    float: right;
+}
+</style>
